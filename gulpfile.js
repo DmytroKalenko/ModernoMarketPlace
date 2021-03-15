6dee2de806e3dc59f509a4d56b5b4dfe27cc5629
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-03-15 19:12:08 +0100

    createHeader

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ var gulp = require('gulp'),
     });
 
     gulp.task('watch', function(){           //auto update file
-        gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+        gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
         gulp.watch('app/*.html', gulp.parallel('html'))
         gulp.watch('app/js/*.js', gulp.parallel('js'))
     })
