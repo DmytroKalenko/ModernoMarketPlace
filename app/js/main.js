fa4661f64bef266cc093afa4a780ac62b368f460
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-03-23 14:38:56 +0100

    change_UserMenu

--- app/js/main.js
+++ app/js/main.js
@@ -23,48 +23,48 @@ $(function() {
     });
     //__Finish_RATEYO___
 
-    //__Start_Header_User__menu__
+    // //__Start_Header_User__menu__
 
-    $('.header__bell, .header__letter, .avatar, .user').hide();
-    //___Start____LOGin_in__site__
+    // $('.header__bell, .header__letter, .avatar, .user').hide();
+    // //___Start____LOGin_in__site__
 
-    $('.login').on('click', function() {
-        $('.header__bell, .header__letter, .avatar, .user').show();
-        $('.login').hide();
-        $('.header__circle li').addClass('curent');
-    });
-
-
-    $('.logout').on('click', function() {
-        $('.header__bell, .header__letter, .avatar, .user').hide();
-        $('.login').show();
-        $('.header__circle li').removeClass('curent');
-    });
-    //___Finish____LOGOUT_in__site__
+    // $('.login').on('click', function() {
+    //     $('.header__bell, .header__letter, .avatar, .user').show();
+    //     $('.login').hide();
+    //     $('.header__circle li').addClass('curent');
+    // });
 
 
-    //__start____Header_User__menu__
-    $('.user').on('click', function() {
-        $('.user').toggleClass("curent");
-        $('.user__list').slideToggle();
-    });
-    $('.user__list .list__item a').on('click', function() {
-        $('.user').toggleClass("curent");
-        $('.user__list').slideToggle();
-    });
-    $('.logout').on('click', function() {
-        $('.user__list, .basket__list').slideUp();
-    });
-    //__Finish____Header_User__menu__
+    // $('.logout').on('click', function() {
+    //     $('.header__bell, .header__letter, .avatar, .user').hide();
+    //     $('.login').show();
+    //     $('.header__circle li').removeClass('curent');
+    // });
+    // //___Finish____LOGOUT_in__site__
 
 
-    //__start____header__basket__
-    $('.header__basket, .basket__list .close').on('click', function() {
-        $('.basket__list').slideToggle();
+    // //__start____Header_User__menu__
+    // $('.user').on('click', function() {
+    //     $('.user').toggleClass("curent");
+    //     $('.user__list').slideToggle();
+    // });
+    // $('.user__list .list__item a').on('click', function() {
+    //     $('.user').toggleClass("curent");
+    //     $('.user__list').slideToggle();
+    // });
+    // $('.logout').on('click', function() {
+    //     $('.user__list, .basket__list').slideUp();
+    // });
+    // //__Finish____Header_User__menu__
 
-    });
 
-    //__finish____header__basket__
+    // //__start____header__basket__
+    // $('.header__basket, .basket__list .close').on('click', function() {
+    //     $('.basket__list').slideToggle();
+
+    // });
+
+    // //__finish____header__basket__
 
 
 });
